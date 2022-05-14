7c6987d56191b9994a88c2ee656c6953b547347e
uuronn <uuronprogram@gmail.com>
2022-05-14 14:40:48 +0900

    Create: LinkText-component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
 import type { NextPage } from "next";
+import { LinkText } from "../components/shared/LinkText";
 
-const Home: NextPage = () => <h1>index page</h1>;
+const Home: NextPage = () => {
+  return (
+    <>
+      <LinkText url="fd">ほーむ</LinkText>
+      <LinkText url="">ほーむ</LinkText>
+      <LinkText url="">ほーむ</LinkText>
+    </>
+  );
+};
 
 export default Home;
